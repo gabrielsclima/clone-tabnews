b67c1dcb71ba037fcdb2940d760a6684c0c6d0fd
Gabriel Souza <gabrielsclima.2004@gmail.com>
2025-01-20 23:38:31 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ function Home() {
         <p>
             Mas agora, quero que saiba que não estou me deixando cair para trás. Pude conhecer muitas pessoas ao longo do caminho que me deixaram de pé e me fortaleceram para me tornar proativo e independente, e ainda estou constantemente progredindo meu conhecimento e experiência. E você também teve participação no meu crescimento desde que eu era menor, onde você foi um grande irmão que me enchia a paciência para falar inglês, e agora consigo entender quase perfeitamente. Fora que sempre apreciei todo o esforço que vinha de você para conseguir crescer, sempre chegando tarde em casa por estar trabalhando, estudando e repetindo esse ciclo toda semana.
         </p>
-        <p>Não tenho nada em mente para te pedir agora, apenas que fique e bem e que seja feliz com a Grace e o Apolo. Quanto a mim, estou querendo seguir os passos parecidos com os seus. O link que você clicou para ler esse texto, é a primeira prova que dou para a vericidade das minhas palavras.</p>
+        <p>Não tenho nada em mente para te pedir agora, apenas que fique bem e que seja feliz com a Grace e o Apolo. Quanto a mim, estou querendo seguir os passos parecidos com os seus. O link que você clicou para ler esse texto, é a primeira prova que dou para a vericidade das minhas palavras.</p>
         <p>Atenciosamente, seu irmão caçula, Gabriel S. C. Lima</p>
     </main>
 }
