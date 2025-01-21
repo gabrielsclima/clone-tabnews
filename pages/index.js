9c4deb36b141c8587e6115d164e6ae6ccfede59a
Gabriel Souza <gabrielsclima.2004@gmail.com>
2025-01-21 02:34:59 +0000

    Adionando a mensagem para meu irmão

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 function Home() {
     return <main>
-        <h1>Aí sim ein</h1>
-        <p>O curso está muito bom!</p>
+        <h1>Fala Brunão!</h1>
+        <p>
+            Você já sabe que não sou de falar muito, mas aqui vou eu kk. Só gostaria de fazer algo que já deveria ter feito há muito tempo, que é agradecer por ter me apoiado em tudo que foi possível. Reconheço que eu poderia ter feito muito mais, mas infelizmente eu não fui forte o suficiente para lidar com minha mente bagunçada para usufruir de tudo ao máximo.
+        </p>
+        <p>
+            Mas agora, quero que saiba que não estou me deixando cair para trás. Pude conhecer muitas pessoas ao longo do caminho que me deixaram de pé e me fortaleceram para me tornar proativo e independente, e ainda estou constantemente progredindo meu conhecimento e experiência. E você também teve participação no meu crescimento desde que eu era menor, onde você foi um grande irmão que me enchia a paciência para falar inglês, e agora consigo entender quase perfeitamente. Fora que sempre apreciei todo o esforço que vinha de você para conseguir crescer, sempre chegando tarde em casa por estar trabalhando, estudando e repetindo esse ciclo toda semana.
+        </p>
+        <p>Não tenho nada em mente para te pedir agora, apenas que fique e bem e que seja feliz com a Grace e o Apolo. Quanto a mim, estou querendo seguir os passos parecidos com os seus. O link que você clicou para ler esse texto, é a primeira prova que dou para a vericidade das minhas palavras.</p>
+        <p>Atenciosamente, seu irmão caçula, Gabriel S. C. Lima</p>
     </main>
 }
 
